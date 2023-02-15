e77f2e27a96b3a651090a377cfb7ab9bbdc93cb1
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-02-15 16:36:41 -0400

    finalizando logica das portas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,25 @@ import Presente from "../../components/Presente"
 import Porta from "../../components/Porta"
 import PortaModel from "model/porta"
 import { useState } from "react"
+import { atualiazarPortas, criarPortas } from "function/porta"
 
 export default function Home() {
 
-  const [p1, setP1] = useState(new PortaModel(1))
-  const [texto, setTexto]= useState("...")
+ 
 
+  const [portas, setPortas]= useState(criarPortas(3,2))
+  
+  function renderizarPortas(){
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} 
+      onChange={novaPorta => setPortas(atualiazarPortas(portas, novaPorta))}
+       />
+    })
+  }
 
   return (
-    <div style={{display:"flex", flexDirection: "column"}}>
-      <input type="text"value={texto} onChange={e => setTexto(e.targe.value)} />
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+    <div style={{display:"flex",}}>
+      {renderizarPortas()}
     </div>
   )
 }
